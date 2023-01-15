99b3e2a7e2e2292e4b4a694a9ae0cd2041d9d42e
Mercurial5 <dias.nespayev@gmail.com>
2023-01-16 03:32:55 +0600

    ref

--- src/contexts/Auth.context.js
+++
@@ -1,7 +0,0 @@
-import axios from 'axios';
-
-export const sendData = async (email, password) => {
-  await axios.post('', { email, password }, { withCredentials: true })
-    .then(res => console.log(res.data))
-    .catch(err => console.error(err))
-}
