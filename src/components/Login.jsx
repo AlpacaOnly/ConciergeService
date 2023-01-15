5f33f13ed5950272c79139b59cb8de42550fe2b3
Mercurial5 <dias.nespayev@gmail.com>
2023-01-16 04:09:06 +0600

    feat: login user

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,51 +1,69 @@
-import React, { useState } from 'react';
+import React, {useState} from 'react';
 import LoginImg from '../assets/amico.png';
-import { Input } from './Form';
-import { sendData } from '../contexts';
+import {Input} from './Form';
+import {login} from "../contexts/api/auth";
+import {me} from "../contexts/api/users";
+import {useNavigate} from "react-router-dom";
 
 export default function Login() {
-	const [email, setLogin] = useState('');
-	const [password, setPassword] = useState('');
+    const [email, setLogin] = useState('');
+    const [password, setPassword] = useState('');
+    const navigate = useNavigate();
 
-	const handleSubmit = async () => {
-		await sendData(email, password);
-	};
+    const LoginHandle = async () => {
 
-	return (
-		<div className='grid grid-cols-1 sm:grid-cols-2 h-screen w-fill'>
-			<div className='hidden sm:block'>
-				<div className='flex justify-center my-64'>
-					<img className='w-4/6 h-4/6 object-cover' src={LoginImg} alt='' />
-				</div>
-			</div>
+        let [data, error] = await login(email, password);
 
-			<div className='bg-gray-800 flex flex-col justify-center'>
-				<form
-					className='max-w-[400px] w-full mx-auto bg-gray-900 p-8 px-8 rounded-lg'
-					onSubmit={(e) => e.preventDefault()}
-				>
-					<h2 className='text-4xl text-white font-bold text-center'>ВОЙТИ</h2>
-					<div className='flex flex-col text-gray-400 py-2'>
-						<Input set={setLogin} label='Логин' />
-					</div>
-					<div className='flex flex-col text-gray-400 py-2'>
-						<Input type='password' label='Пароль' set={setPassword} />
-					</div>
-					<div className='flex justify-between text-gray-400 py-2'>
-						<p className='flex items-center'>
-							<input className='mr-2' type='checkbox' /> Запомнить{' '}
-						</p>
-						<p>Забыли пароль?</p>
-					</div>
-					<button
-						className='w-full my-5 py-2 bg-teal-500 shadow-lg shadow-teal-500/50 hover:shadow-teal-500/20 text-white font-semibold rounded-lg'
-						onClick={handleSubmit}
-					>
-						Войти
-					</button>
-				</form>
-			</div>
-		</div>
-	);
+        if (error) {
+            if (error['response']['status'] === 400) alert('Неправильный логин или пароль');
+        } else {
+            let token = data['auth_token']
+            localStorage.setItem('token', token);
+
+            const {response} = await me(token)
+
+            if (response) {
+                localStorage.setItem('user', JSON.stringify(response));
+                navigate('/');
+            }
+        }
+    };
+
+    return (
+        <div className='grid grid-cols-1 sm:grid-cols-2 h-screen w-fill'>
+            <div className='hidden sm:block'>
+                <div className='flex justify-center my-64'>
+                    <img className='w-4/6 h-4/6 object-cover' src={LoginImg} alt=''/>
+                </div>
+            </div>
+
+            <div className='bg-gray-800 flex flex-col justify-center'>
+                <form
+                    className='max-w-[400px] w-full mx-auto bg-gray-900 p-8 px-8 rounded-lg'
+                    onSubmit={(e) => e.preventDefault()}
+                >
+                    <h2 className='text-4xl text-white font-bold text-center'>ВОЙТИ</h2>
+                    <div className='flex flex-col text-gray-400 py-2'>
+                        <Input set={setLogin} label='Логин'/>
+                    </div>
+                    <div className='flex flex-col text-gray-400 py-2'>
+                        <Input type='password' label='Пароль' set={setPassword}/>
+                    </div>
+                    <div className='flex justify-between text-gray-400 py-2'>
+                        <p className='flex items-center'>
+                            <input className='mr-2' type='checkbox'/> Запомнить{' '}
+                        </p>
+                        <p>Забыли пароль?</p>
+                    </div>
+                    <button
+                        className='w-full my-5 py-2 bg-teal-500 shadow-lg shadow-teal-500/50 hover:shadow-teal-500/20 text-white font-semibold rounded-lg'
+                        onClick={LoginHandle}
+                    >
+                        Войти
+                    </button>
+                </form>
+            </div>
+        </div>
+    );
 }
 
--- src/contexts/api/users/me.js
+++ src/contexts/api/users/me.js
@@ -1,15 +1,16 @@
 import axios from 'axios';
 
 export const me = async (token) => {
-    let data, error;
 
     const config = {
         headers: {'Authorization': 'Token ' + token}
     }
 
-    await axios.get('http://127.0.0.1:8000/api/users/me/', config)
-        .then(res => data = res.data)
-        .catch(err => error = err)
-
-    return [data, error];
+    return axios.get('http://127.0.0.1:8000/api/users/me/', config)
+        .then(function (response) {
+            return {response: response.data};
+        })
+        .catch(function (error) {
+            return error;
+        })
 }
