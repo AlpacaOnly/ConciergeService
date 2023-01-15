75ec0ff2e49e4197e47d6f69288997696d03ceab
Mercurial5 <dias.nespayev@gmail.com>
2023-01-16 04:58:32 +0600

    ref: warning

--- src/pages/ProfileRouter.js
+++ src/pages/ProfileRouter.js
@@ -5,6 +5,6 @@ export default function ProfileRouter({user}) {
     switch (user.role) {
         case 'manager': return <ManagerProfile/>
         case 'client':  return <ClientProfile/>
+        default: return null;
     }
-    return <h1>This is profile Router</h1>
 }
